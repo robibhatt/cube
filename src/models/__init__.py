db074713e259b5291e6027f4ed412c1430b7c509
robibhatt <robibhatt@gmail.com>
2025-10-09 11:59:14 +0200

    Type check Fourier detector MLP input

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -2,6 +2,12 @@
 
 from .mlp import MLP
 from .mlp_config import MLPConfig
+from .mlp_fourier_detector import detect_mlp_fourier_components
 from .mlp_utils import export_neuron_input_gradients
 
-__all__ = ["MLP", "MLPConfig", "export_neuron_input_gradients"]
+__all__ = [
+    "MLP",
+    "MLPConfig",
+    "detect_mlp_fourier_components",
+    "export_neuron_input_gradients",
+]
