4e8f912f20ceda02be469edbd7346271a4e656a2
robibhatt <robibhatt@gmail.com>
2025-10-31 10:53:53 +0100

    Refactor MLP sparsification utilities into focused modules

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -2,11 +2,15 @@
 
 from src.models.mlp import MLP
 from src.models.mlp_config import MLPConfig
-from src.models.sparsify_mlp import binary_search_sparsify_threshold, sparsify_mlp
+from src.models.pruned_mlp import prune, visualize_pruned_mlp
+from src.models.sparsify import binary_search_sparsify_threshold, mse_diff, sparsify_mlp
 
 __all__ = [
     "MLP",
     "MLPConfig",
+    "mse_diff",
     "binary_search_sparsify_threshold",
     "sparsify_mlp",
+    "prune",
+    "visualize_pruned_mlp",
 ]
